dd658912c31a304218291070b17f20341cb99ec7
Felix Kling <felix.kling@gmx.net>
2013-03-24 15:19:22 +0000

    Add examples.

--- javascripts/examples.js
+++ javascripts/examples.js
@@ -1,68 +1,54 @@
-"use strict";
-/*globals jsnx*/
-/*exported examples*/
-var examples = (function() {
-  function random(min, max) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
+/*jshint jquery:true, browser: true*/
+/*global d3*/
+(function() {
+  "use strict";
+  var $examples = $('.example');
+  $examples.each(function(i) {
+    var link = '#' + i;
+    $(this).find('h4').prepend('<a href="' + link + '">' + link + ':&nbsp;</a>');
+  });
+
+  var COMMENT = /^\/\//;
+  var CONSOLE = /^console/;
+  function extract_code(example) {
+    return $(example).find('li').map(function() {
+      var text = $(this).text();
+      if (!COMMENT.test(text) && !CONSOLE.test(text)) {
+        text = text.replace(/\/\/.*$/, '');
+        return text;
+      }
+    }).get().join('');
   }
 
-  return [
-    function(cb) {
-      var n = random(3, 8);
-      var G = jsnx.complete_graph(n);
-      cb(
-        G,
-        'Complete graph (nodes: ' + n + ')',
-        {
-          layout_attr: {
-            charge: -120,
-            linkDistance: 100
+  var $description = $('#canvas > .description');
+  function draw(example) {
+    var $example = $(example);
+    $description.text($example.children('h4').text());
+    (new Function(extract_code(example))());
+    d3.selectAll('#canvas svg').style('opacity', 0.01).transition().duration(1500)
+    .style('opacity', 1);
+  }
+
+  // Find top most visible example and draw it
+  var timer;
+  var last = null;
+  $(document).on('scroll', function() {
+    window.clearTimeout(timer);
+    timer = setTimeout(function() {
+      var cutoff = $(window).scrollTop();
+      var half_height = $(window).height() / 2;
+      $examples.each(function() {
+        var top = $(this).offset().top;
+        if (top > cutoff && top < cutoff + half_height) {
+          if (last !== this) {
+            $(last).removeClass('active');
+            $(this).addClass('active');
+            last = this;
+            draw(this);
           }
+          return false;
         }
-      );
-    },
-    function(cb) {
-      var r = random(2, 6);
-      var c = random(2, 6);
-      var G = jsnx.grid_2d_graph(r, c);
-      cb(G, 'Grid 2d graph (rows: ' + r + ', columns: ' + c + ')');
-    },
-    function(cb) {
-      var n = random(4, 20);
-      var G = jsnx.cycle_graph(n);
-      cb(G, 'Cycle graph (nodes: ' + n + ')');
-    },
-    function(cb) {
-      var b = random(2,5);
-      var n = random(5, 20);
-      var G = jsnx.full_rary_tree(b, n);
-      cb(
-        G,
-        'Full rary tree (branch factor: ' + b + ', nodes: ' + n + ')',
-        {
-          layout_attr: {
-            charge: -100,
-            linkDistance: 20
-          }
-        }
-      );
-    },
-    function(cb) {
-      var n = random(5,20);
-      var G = jsnx.fast_gnp_random_graph(n, 0.3);
-      cb(G, 'Random graph (nodes: ' + n + ', edge probability: 0.3)');
-    },
-    function(cb) {
-      cb(
-        jsnx.karate_club_graph(),
-        'Karate club graph',
-        {
-          layout_attr: {
-            charge: -400,
-            linkDistance: 60
-          }
-        }
-      );
-    }
-  ];
+      });
+    }, 50);
+  }).trigger('scroll');
 }());
