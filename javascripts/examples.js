865b6febda440143e26d634f57132b10b3d3ba63
Felix Kling <felix.kling@gmx.net>
2013-03-29 00:32:03 +0000

    Improve graph drawing/positioning.

--- javascripts/examples.js
+++ javascripts/examples.js
@@ -4,9 +4,20 @@
   "use strict";
   var $examples = $('.example');
   $examples.each(function(i) {
-    var link = '#' + i;
-    $(this).find('h4').prepend('<a href="' + link + '">' + link + ':&nbsp;</a>');
+    this.id = "example" + i;
+    $(this).find('h4').prepend('<a href="#example' + i + '">#' + i + ':&nbsp;</a>');
   });
+
+
+  // Compute position of canvas based on screen size
+  var bottom = $('#header').offset().top + $('#header').height();
+  var available_height = $(window).height() - bottom;
+  $('#canvas').css({
+    'position': 'fixed',
+    'height': available_height * 0.6,
+    'top': bottom + 0.1 * available_height
+  });
+
 
   var COMMENT = /^\/\//;
   var CONSOLE = /^console/;
@@ -32,23 +43,45 @@
   // Find top most visible example and draw it
   var timer;
   var last = null;
-  $(document).on('scroll', function() {
+  var dheight = $(document).height();
+  var handler = function(event, force) {
     window.clearTimeout(timer);
     timer = setTimeout(function() {
       var cutoff = $(window).scrollTop();
       var half_height = $(window).height() / 2;
-      $examples.each(function() {
-        var top = $(this).offset().top;
-        if (top > cutoff && top < cutoff + half_height) {
-          if (last !== this) {
+      if (cutoff + half_height * 2 >= dheight) {
+        // bottom, show last example
+        var l = $examples.last().get(0);
+        if (l !== last || force) {
             $(last).removeClass('active');
-            $(this).addClass('active');
-            last = this;
-            draw(this);
-          }
-          return false;
+            $(l).addClass('active');
+            last = l;
+            draw(l);
         }
-      });
+      }
+      else {
+        $examples.each(function() {
+          var top = $(this).offset().top;
+          var center = top + $(this).height() / 2;
+          if (center > cutoff && top < cutoff + half_height) {
+            if (last !== this || force) {
+              $(last).removeClass('active');
+              $(this).addClass('active');
+              last = this;
+              draw(this);
+            }
+            return false;
+          }
+        });
+      }
     }, 50);
-  }).trigger('scroll');
+  };
+
+  window.onhashchange = function() {
+    $(document).trigger('scroll');
+  };
+  $(document).on('scroll', handler).trigger('scroll');
+  $(window).resize(function() {
+    handler(null, true);
+  });
 }());
--- javascripts/graph.js
+++ javascripts/graph.js
@@ -1,59 +1,69 @@
-/*jshint browser:true*/
+/*jshint browser:true, jquery:true*/
 /*global jsnx:true, d3:true*/
 (function() {
   "use strict";
-  var color = d3.scale.category20();
   var G;
-  if (Math.random() <= 0.2) {
-    // NetworkX graph
-    G = jsnx.Graph();
-    G.add_nodes_from([1,2,3,4], {group:1});
-    G.add_nodes_from([5,6,7], {group:2});
-    G.add_nodes_from([8,9,10,11], {group:3});
+  var tick;
+  function draw() {
+    clearTimeout(tick);
+    var color = d3.scale.category20();
+    if (Math.random() <= 0.2) {
+      // NetworkX graph
+      G = jsnx.Graph();
+      G.add_nodes_from([1,2,3,4], {group:1});
+      G.add_nodes_from([5,6,7], {group:2});
+      G.add_nodes_from([8,9,10,11], {group:3});
 
-    G.add_path([1,2,5,6,7,8,11]);
-    G.add_edges_from([
-     [1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,9]
-    ]);
-  }
-  else {
-    color = d3.scale.category10();
-    var edges = jsnx.binomial_graph(
-      Math.floor((Math.random() * 11) + 10),
-      0.12
-    ).edges();
-    G = jsnx.Graph();
-    (function tick() {
-      if (edges.length) {
-        G.add_edge.apply(G, edges.shift());
-        setTimeout(tick, 1800);
-      }
-    }());
-  }
+      G.add_path([1,2,5,6,7,8,11]);
+      G.add_edges_from([
+                       [1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,9]
+      ]);
+    }
+    else {
+      color = d3.scale.category10();
+      var edges = jsnx.binomial_graph(
+        Math.floor((Math.random() * 11) + 10),
+        0.12
+      ).edges();
+      G = jsnx.Graph();
+      (function t() {
+        if (edges.length) {
+          G.add_edge.apply(G, edges.shift());
+          tick = setTimeout(t, 1800);
+        }
+      }());
+    }
 
-  d3.select('#header-chart').style('opacity', 0.01)
-  .transition().style('opacity', 1);
+    d3.select('#header-chart').style('opacity', 0.01)
+    .transition().style('opacity', 1);
 
-  jsnx.draw(G, {element: '#header-chart',
-            layout_attr: {
-              charge: -100,
-              linkDistance: 20,
-              gravity: 0.3
-            },
-            pan_zoom: {
-              enabled: false
-            },
-            node_attr: {
-              r: 4
-            },
-            node_style: {
-              fill: function(d) {
-                return color(d.data.group || +d.node % 4);
+    jsnx.draw(G, {element: '#header-chart',
+              layout_attr: {
+                charge: -100,
+                linkDistance: 20,
+                gravity: 0.3
               },
-              stroke: 'none'
-            },
-            edge_style: {
-              fill: '#999'
-            }
-  }, true);
+              pan_zoom: {
+                enabled: false
+              },
+              node_attr: {
+                r: 4
+              },
+              node_style: {
+                fill: function(d) {
+                  return color(d.data.group || +d.node % 4);
+                },
+                stroke: 'none'
+              },
+              edge_style: {
+                fill: '#999'
+              }
+    }, true);
+  }
+  draw();
+  var timer;
+  $(window).resize(function() {
+    clearTimeout(timer);
+    timer = setTimeout(draw, 300);
+  });
 }());
