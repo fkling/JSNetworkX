d2b73403e83ad29a66bda01431f0707c8c6c9cdc
Felix Kling <felix.kling@gmx.net>
2015-04-11 09:44:25 -0700

    Final update for v0.3.0

---
+++ scripts/update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp ../../jsnetworkx/jsnetworkx.js ../javascripts/
+./updateAPIPage.sh
--- scripts/updateAPIPage.sh
+++ scripts/updateAPIPage.sh
@@ -5,6 +5,6 @@ API_REPO=../jsnetworkx-api
 rm -rf api/
 cd $API_REPO
 npm run build
-# npm run gen-api
+npm run gen-api
 cd -
 cp -R $API_REPO/website ./api
